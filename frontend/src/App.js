475f5b88e0524b596a13be29293a2ef1ac597d6c
alphacpc <alphacpc@gmail.com>
2022-03-26 11:21:58 +0000

    Basic App working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import * as React from "react";
+import { Routes, Route} from "react-router-dom";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Salam</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const {Client} = require("@elastic/elasticsearch");
 const app = express();
-const port = 4000;
+const port = 4040;
 
 // MIDDLEWARES
 app.use(cors());
